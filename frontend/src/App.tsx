d9405de90d39acc9be7e09b68412c418807c76d2
Taz Choudhury <tazzcn@gmail.com>
2025-04-21 19:44:50 +0100

    feat: add Table component and integrate it into the main App layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,14 @@
-import { Box } from "@mui/material"
-import { Header } from "./components/Header"
+import { Box } from "@mui/material";
+import { Header } from "./components/Header";
+import { Table } from "./components/Table";
 
 function App() {
-
   return (
-    <Box sx={{height: '100vh', width: '100vw'}}>
+    <Box sx={{ height: "100vh", width: "100vw" }}>
       <Header />
+      <Table />
     </Box>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/components/Table/Table.tsx
@@ -0,0 +1,10 @@
+import { Box } from "@mui/material";
+
+const Table = () => {
+  return (
+    <Box sx={{ flexGrow: 1, padding: 2 }}>
+      <h1>Table</h1>
+    </Box>
+  );
+};
+export default Table;
